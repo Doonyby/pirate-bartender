bbdafdfda7cff43cfa4a1e8444e9eaca07b1da21
Dan Riding <doonyby@gmail.com>
2016-06-28 22:17:00 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+//Get the customer's name.
+var name = prompt("What is your name");
+
+
+//Constructor function for a new customer that accepts argument of what the customer consumes.
+var Customer = function(seat) {
+	this.seat = seat;
+}
+
+
+
+
+});
